4cd6d26425774b8d95ea404a4978a172eb4c4aa1
Varchas H V <hvvarchas@gmail.com>
2026-04-30 00:18:51 +0530

    re-implementation

---
+++ tiny_shell/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//lex it like u mean it
+enum class State{
+  NORMAL,
+  IN_QUOTE,
+  IN_WORD,
+  PIPE,
+  SPECIAL, //cause u can't put a fucking space
+  END
+}
+
+int main(){
+  while (1){
+    string line;
+    cout<<"nya>>";
+    //read input
+    getline(cin, line);
+
+
+  }
+return 0;
+}
+
+
+
